cfd18f8d2e21f0aee9d3373606a5a1a8ed4bd429
imgokulkaish <gokulkanishbe@gmail.com>
2022-11-08 10:37:02 +0530

    initial-commit

--- src/Pages/Checkout/Checkout.jsx
+++ src/Pages/Checkout/Checkout.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import "./Checkout.css";
+import SubTotal from '../../components/SubTotal/SubTotal';
+import CheckoutProduct from '../../components/CheckoutProduct/CheckoutProduct';
+import { useStateValue } from '../../components/StateProvider/StateProvider'
 const Checkout = () => {
+    const [{ basket }] = useStateValue()
     return (
         <div>
             <Header />
-            <h1>Checkout!</h1>
+            <div className="checkout">
+                <div className="checkout__left">
+                    <img
+                        className="checkout__ad"
+                        src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                        alt=""
+                    />
+                    {basket?.length === 0 ? (
+                        <div>
+                            <h2>Your Shopping Basket is empty</h2>
+                            <p>
+                                You have no items in your basket. To buy or more items, click "Add
+                                to basket" next to the item.
+                            </p>
+                        </div>
+                    ) : (
+                        <div>
+                            <h2 className="checkout__title">Your shopping basket</h2>
+                            {basket.map((item) => (
+                                <CheckoutProduct
+                                    id={item.id}
+                                    title={item.title}
+                                    image={item.image}
+                                    price={item.price}
+                                    rating={item.rating}
+                                />
+                            ))}
+                        </div>
+                    )}
+                </div>
+                {basket.length > 0 && (
+                    <div className="checkout__right">
+                        <SubTotal />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
--- src/components/StateProvider/reducer.js
+++ src/components/StateProvider/reducer.js
@@ -3,12 +3,34 @@ export const initialState = {
   user: null,
 }
 
+export const getBasketTotal = (basket) => {
+  return basket?.reduce((amount, item) => item.price + amount, 0)
+}
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_USER':
       return {
         ...state,
         user: action.user,
+      }
+    case 'ADD_TO_BASKET':
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      }
+    case 'REMOVE_FROM_BASKET':
+      let newBasket = [...state.basket]
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id,
+      )
+      if (index >= 0) {
+        newBasket.splice(index, 1)
+      } else {
+        console.warn("Can't find index")
+      }
+      return {
+        ...state,
+        basket: newBasket,
       }
     default:
       return state
